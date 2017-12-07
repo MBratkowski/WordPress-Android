f66b9bce543a583184a41fcd57d106fcd788e6bc
Alex Forcier <alex.forcier@automattic.com>
2017-12-07 14:56:40 +1100

    Set up custom GlideModule using FluxC's RequestQueue

--- WordPress/src/main/java/org/wordpress/android/modules/AppComponent.java
+++ WordPress/src/main/java/org/wordpress/android/modules/AppComponent.java
@@ -248,6 +248,8 @@ public interface AppComponent extends AndroidInjector<WordPress> {
     void inject(PluginListActivity object);
     void inject(PluginDetailActivity object);
 
+    void inject(WordPressGlideModule object);
+
     // Allows us to inject the application without having to instantiate any modules, and provides the Application
     // in the app graph
     @Component.Builder
